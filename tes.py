faa51e296a50ebe6acb280635a774684ce84398b
imagecser <lestdawn@163.com>
2016-11-28 16:24:00 +0800

    test

--- tes.py
+++ tes.py
@@ -3,14 +3,20 @@
 import sys
 import urllib2
 import traceback
+import cookielib
+from lxml import etree
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-url = 'http://jw.nju.edu.cn/'
-try:
-    response = urllib2.urlopen(url)
-    print response.read()
-except:
-    trace = traceback.format_exc()
-    print trace
-    print trace.count('timed out')
+new = []
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
+opener.addheaders = [
+    ('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0')]
+url = 'file:///C:/Users/Administrator/Desktop/x.html'
+page = etree.HTML(opener.open(url, timeout=10).read())
+ltitle = page.xpath('//div/table/tbody/tr/td/ul/li/div/span/a/@title')
+lhref = page.xpath('//div/table/tbody/tr/td/ul/li/div/span/a/@href')
+for i in range(len(ltitle)):
+    print ltitle[i]
+    print lhref[i]
+
