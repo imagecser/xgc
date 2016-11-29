c5c6bc222c7e7b9476da6b1ea26e2198546c34fa
imagecser <lestdawn@163.com>
2016-11-29 17:46:54 +0800

    move into my own website: http://www.icser.me

--- onetp.py
+++ onetp.py
@@ -5,6 +5,7 @@ import urllib
 import time
 import os
 import re
+import uuid
 import traceback
 from i_log import *
 from i_send import *
@@ -17,72 +18,51 @@ sys.setdefaultencoding('utf-8')
 
 cerurl = 'http://cer.nju.edu.cn/amserver/UI/Login'
 capurl = 'http://cer.nju.edu.cn/amserver/verify/image.jsp'
-maddr= ['lestdawn@163.com']
+maddr = ['lestdawn@163.com']
 logfile, postfile = 'main.log', 'post.log'
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
 
 
 def get_th():
-    itry, iloginout, ivisitout, ivisitun, itwe = 0, 0, 0, 0, 0
+    itry = 0
     while True:
         global opener
         opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookielib.CookieJar()))
         opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0')]
+        # 构建CookieJar Opener
         try:
             cap_content = opener.open(capurl, timeout=10).read()
             open('image.jpg', 'wb').write(cap_content)
             capocr = image_to_string(Image.open('image.jpg'))
             cap = capocr.replace('\n', '').replace(' ', '')
+            # 打开验证码网页，保存验证码，OCR转化
             data = urllib.urlencode({'IDToken1': '9405257',
                                      'IDToken2': 'nju697196',
                                      'inputCode': cap,
                                      'encoded': 'false'})
-            try:
-                html = opener.open(cerurl, data, timeout=10).read()
-                itry += 1
-                if len(html) > 2000:
-                    try:
-                        page = opener.open('http://xgc.nju.edu.cn/xg/main.psp', timeout=10).read()
-                        log(logfile, "Authentication for [%d] times" % itry, 'INFO')
-                        return page
-                    except:
-                        trace = traceback.format_exc()
-                        if trace.count('timed out') > 0:
-                            ivisitout += 1
-                            if ivisitout > 5:
-                                log(logfile, '[xgc url] timed out\n' + trace, 'ERROR')
-                                break
-                        else:
-                            ivisitun += 1
-                            if ivisitun > 5:
-                                log(logfile, '[xgc url] error unknown\n' + trace, 'ERROR')
-                                break
-                if ivisitout == 0:
-                    if itry >= 20:
-                        if itwe == 0:
-                            log(logfile, "Authentication for over [20] times", 'WARNING')
-                            itwe = 1
-                    if itry >= 50:
-                        log(logfile, "Authentication for over [50] times. Quited.", 'WARNING')
-                        break
-            except:
-                trace = traceback.format_exc()
-                if trace.count('timed out') > 0:
-                    iloginout += 1
-                    if iloginout > 5:
-                        log(logfile, '[login url]\n' + trace, 'ERROR')
-                        break
-                    else:
-                        continue
+            html = opener.open(cerurl, data, timeout=10).read()
+            itry += 1
+            # 携带headers尝试登陆身份验证页面
+            if len(html) > 2000:
+                page = opener.open('http://xgc.nju.edu.cn/xg/main.psp', timeout=10).read()
+                log(logfile, "Authentication for [%d] times" % itry, 'INFO')
+                return page
+                # 携带登陆成功的cookies,登陆TMD学工处主页，以字节数判断验证是否成功
+            if itry >= 32:
+                log(logfile, "Authentication for over [32] times. Quited.", 'WARNING')
+                break
+                # 登陆超过2**5次失败，暂时跳过，等待下一次重新尝试
         except:
             trace = traceback.format_exc()
-            log(logfile, '[captcha url]\n' + trace, 'ERROR')
+            log(logfile, trace, 'ERROR')
             break
+            # 错误返回traceback信息
     return ''
 
 
 def rewrite(post_read):
     page = re.findall(re.compile('h.+htm'), post_read)
+    # 匹配所有网址为list
     opener.addheaders = [('User-agent', 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0')]
     for iurl in range(len(page)):
         try:
@@ -96,14 +76,16 @@ def rewrite(post_read):
                               'href="http://xgc.nju.edu.cn/').replace('src="/',
                                                                       'src="http://xgc.nju.edu.cn/').replace(
                 "src='/", "src='http://xgc.nju.edu.cn/").replace("'src', '/", "'src', 'http://xgc.nju.edu.cn/")
-            name = str(int(time.time()))
+            name = str(uuid.uuid1())
             rewpath = 'C:\\inetpub\\wwwroot\\xgc\\c' + name + '.html'
             open(rewpath, 'w').write(rew)
             post_read = post_read.replace(page[iurl], 'http://www.icser.me/xgc/c' + name + '.html')
             time.sleep(0.1)
         except:
-            log('main.log', 'rewrite error.' + page[iurl], 'ERROR')
+            log(logfile, page[iurl], 'ERROR')
     return post_read
+    # 依次获得每个网页的源码，保留标题、所需的正文，填补并改善html格式，将超链接的相对路径换位绝对路径，
+    # 将改写的网页存入网站服务器/xgc/，以uuid.uuid1()命名
 
 
 def comp(page):
@@ -119,27 +101,33 @@ def comp(page):
         res = [i for i in new if i not in old]
         finfo.writelines(res)
         return res
+        # 构造新旧信息的list：旧[已存文件读取]，新[xpath从get_th()中获得]、list结构有改进空间，懒了
+        # 比较得出新信息，将新信息写入，返回list
     else:
         return []
 
 
 def join():
     post, pare = '', comp(get_th())
+    # 构造空字符串post，待添加发送的信息；pare即返回的list
     if os.path.exists('post_temp.log'):
-        post += open('post_temp.log', 'r').read()
-        os.remove('post_temp.log')
+        post += open('post_temp.txt', 'r').read()
+        os.remove('post_temp.txt')
+        # 添加曾未发送的信息，删除暂时性文档
     for item in pare:
         post += item
     if post.replace(' ', '').replace('\n', '') != '':
         post = post.replace('\n\n', '\n')
+        # 写入新信息，准备重写为外部网址
         post = rewrite(post)
         try:
             send_(maddr, post, u"学工园地更新动态")
             log(postfile, post, 'INFO')
         except:
             trace = traceback.format_exc()
-            open('post_temp.log', 'a').write(post)
-            log(logfile, '[email sending]\n' + trace, 'ERROR')
+            open('post_temp.txt', 'a').write(post)
+            log(logfile, trace, 'ERROR')
+            # 将未发送成功的信息暂时存储
 
 if __name__ == "__main__":
     while True:
@@ -150,4 +138,5 @@ if __name__ == "__main__":
             log(logfile, trace, 'ERROR')
         finally:
             opener.close()
+
             time.sleep(1800)
--- tes.py
+++ tes.py
@@ -4,7 +4,15 @@ import sys
 import urllib2
 import traceback
 import cookielib
+import chardet
+import time
 from lxml import etree
+import uuid
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+x = []
+time1 = time.time()
+for i in range(10000):
+    x.append(uuid.uuid1())
+print time.time() - time1
